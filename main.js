566942efa0021295cea0fe963231b3f033287517
Vovan1608 <vovan1608@gmail.com>
2021-03-17 19:40:38 +0200

    added serializeClockTime, civilianHours

--- main.js
+++ main.js
@@ -4,3 +4,18 @@ const oneSecond = () => 1000;
 const getCurrentTime = () => new Date();
 const clear = () => console.clear();
 const log = (message) => console.log(message);
+
+// получает объект времени и возвращает объект для
+// показания часов, содержащих часы, минуты и секунды
+const serializeClockTime = (date) => ({
+  hours: date.getHours(),
+  minutes: date.getMinutes(),
+  seconds: date.getSeconds(),
+});
+
+// получает объект показания часов и возвращает объект, где
+// показание часов преобразовано к формату гражданского времени.
+const civilianHours = (clockTime) => ({
+  ...clockTime,
+  hours: clockTime.hours > 12 ? clockTime.hours - 12 : clockTime.hours,
+});
